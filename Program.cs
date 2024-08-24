40c21cf8e907f5ea480a9864bd77f202d38da909
mailsonpuc <mailson.costa@proton.me>
2024-08-23 22:24:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 Console.WriteLine("Teste no Nokia");
-Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "9876", memoria: 128);
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "123456789023456", memoria: 128);
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("WhatsApp");
@@ -10,7 +10,7 @@ nokia.InstalarAplicativo("WhatsApp");
 Console.WriteLine("..............");
 
 Console.WriteLine("Teste no Iphone");
-Smartphone iphone = new Iphone(numero: "1357", modelo: "modelo 3", imei: "1234", memoria: 32);
+Smartphone iphone = new Iphone(numero: "1357", modelo: "modelo 3", imei: "123455555555555", memoria: 32);
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Youtube");
